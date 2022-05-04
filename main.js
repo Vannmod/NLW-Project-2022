0820cc077c4b983b5950ba1f161b0ffdfe024762
Vannmod <rafaelbattglin94@gmail.com>
2022-05-04 17:27:45 -0300

    Padronização, eventos de clique, seção serviços, rolagem suave,sobreposição de menu, o menu, propriedades e funcionalidades no javascript

--- main.js
+++ main.js
@@ -9,7 +9,22 @@ function onScroll() {
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
-
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+
+ScrollReveal({
+  origin: 'top',
+  distance:'30px',
+  duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .cards,
+#about,
+#about header,
+#about .content`);
