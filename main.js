772535795399a805c51800bf761cae978cb38dd6
Vannmod <rafaelbattglin94@gmail.com>
2022-05-07 15:56:45 -0300

    Transformação do site para telas maiores

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-window.addEventListener('scroll',onScroll )
-
+window.addEventListener('scroll', onScroll)
+//2:25:16
 onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  console,console.log(menuElement);
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
@@ -22,7 +56,6 @@ function showBackToTopButtonOnScroll() {
   }
 }
 
-
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
@@ -30,11 +63,10 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
-
 ScrollReveal({
   origin: 'top',
-  distance:'30px',
-  duration: 700,
+  distance: '30px',
+  duration: 700
 }).reveal(`
 #home, 
 #home img, 
@@ -44,4 +76,4 @@ ScrollReveal({
 #services .cards,
 #about,
 #about header,
-#about .content`);
+#about .content`)
